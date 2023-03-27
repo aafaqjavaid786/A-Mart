4bcf350a789273aaea5207cc9aa506913fafd99c
AJ <aafaqjavaid786@gmail.com>
2023-03-27 15:08:34 +0500

    Backend User Authentication

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
-from .models import Product
-from .serializers import ProductSerializer
-from .products import products
 
-# Create your views here.
+from rest_framework import status
 
-@api_view(['GET'])
-def getRoutes(request):
-    return Response("Hello")
+from ..models import Product
+from ..serializers import ProductSerializer
+from ..products import products
 
 @api_view(['GET'])
 def getProducts(request):
